d0da5345c1d79db496bbe2b14622e4548cc1c90f
Facundo <facundo.arnold@gmail.com>
2022-07-16 01:04:17 -0300

    first commit

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]               // api/autores
+        [HttpGet("listado")]    // api/autores/listado
+        [HttpGet("/listado")]   // listado
+        public  List<Autor> Get()
+        {
+            return context.Autores.Include(x => x.Libros).ToList();
+        }
+
+        [HttpGet("{id:int}/{param2=persona}")] //param2? puede ser opcional | param2=persona valor por default
+        public async Task<ActionResult<Autor>> Get(int id,string param2) {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return autor;
+            }
+        }
+        
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get(string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return autor;
+            }
+        }
+
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> PrimerAutor()
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor) {
+
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id) {
+
+            if (autor.Id != id) {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id) {
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+            
+        }
+
+    }
+}
